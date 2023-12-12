b216dfd6e1b5cfdd8365bb5700b0af970a29bf9b
Caio Marques Araujo da Silva <caio.mas@uol.com.br>
2023-12-11 21:12:37 -0300

    Adding App.jsx

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Page from './components/sections/Page.jsx'
 import "./global.scss";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Page />
+
   </React.StrictMode>,
 )
